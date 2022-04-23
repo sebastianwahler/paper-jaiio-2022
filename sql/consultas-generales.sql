205fde792b8319972786074151424991858a5e2a
Sebastián Pablo Wahler <sebastian.wahler@gmail.com>
2022-04-22 23:30:54 -0300

    se agregó tabla con valores extraídos de la DB. Se creo carpeta sql para scripts

---
+++ sql/consultas-generales.sql
@@ -0,0 +1,18 @@
+use Coiron
+--CantNodos
+select COUNT (*) from RedGPNodo
+--CantEnlaces
+select COUNT (*) from RedGPEnlace 
+--Casos
+select COUNT (*) from  RedGPNodoRelacionEnlace
+--CantEnlaces
+select COUNT (*) from RedGPEnlace 
+
+select *FROM RedGPNodo N
+
+select *FROM RedGPEnlace E
+
+select *FROM RedGPNodoRelacionEnlace R
+
+
+
